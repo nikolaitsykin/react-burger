46faa2e9e2f13eb4ed17bcb8f0bb39798724725c
flipexe <104075088+flipexe@users.noreply.github.com>
2023-03-20 10:59:23 -0400

    Order details page fixed

--- src/pages/feed/feed-details/feed-details.tsx
+++ src/pages/feed/feed-details/feed-details.tsx
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { FeedDetails } from "../../../components/Feed/FeedDetails/FeedDetails";
 import { useActions } from "../../../hooks/actions";
+import { useGetIngredientsQuery } from "../../../services/store/api";
 import { getOrders } from "../../../services/store/reducers/ws.slice";
 
 export const FeedDetailsPage = () => {
@@ -12,6 +13,29 @@ export const FeedDetailsPage = () => {
       close();
     };
   }, [open, close]);
+
+  const { getIngredients, getIngredientsFailed } = useActions();
+
+  const {
+    isError: isIngredientsError,
+    data: ingredients,
+    isSuccess: isIngredientsGetSuccess,
+  } = useGetIngredientsQuery("");
+
+  useEffect(() => {
+    if (isIngredientsGetSuccess) {
+      getIngredients(ingredients);
+    }
+    if (isIngredientsError) {
+      getIngredientsFailed();
+    }
+  }, [
+    ingredients,
+    isIngredientsGetSuccess,
+    isIngredientsError,
+    getIngredients,
+    getIngredientsFailed,
+  ]);
 
   return <FeedDetails />;
 };
