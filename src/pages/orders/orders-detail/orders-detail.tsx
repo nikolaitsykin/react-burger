7dee59ec9de0211add9324f91f1142e3670c0d0a
flipexe <104075088+flipexe@users.noreply.github.com>
2023-03-20 11:54:25 -0400

    Minor fix

--- src/pages/orders/orders-detail/orders-detail.tsx
+++ src/pages/orders/orders-detail/orders-detail.tsx
@@ -1,10 +1,34 @@
 import { useEffect } from "react";
 import { FeedDetails } from "../../../components/Feed/FeedDetails/FeedDetails";
 import { useActions } from "../../../hooks/actions";
+import { useGetIngredientsQuery } from "../../../services/store/api";
 import { getPersonalOrders } from "../../../services/store/reducers/ws.slice";
 
 export const OrderDetailsPage = () => {
   const { open, close } = useActions();
+
+  const { getIngredients, getIngredientsFailed } = useActions();
+
+  const {
+    isError: isIngredientsError,
+    data: ingredients,
+    isSuccess: isIngredientsGetSuccess,
+  } = useGetIngredientsQuery("");
+
+  useEffect(() => {
+    if (isIngredientsGetSuccess) {
+      getIngredients(ingredients);
+    }
+    if (isIngredientsError) {
+      getIngredientsFailed();
+    }
+  }, [
+    ingredients,
+    isIngredientsGetSuccess,
+    isIngredientsError,
+    getIngredients,
+    getIngredientsFailed,
+  ]);
 
   useEffect(() => {
     open(getPersonalOrders());
